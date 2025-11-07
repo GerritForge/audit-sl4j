608eff529f36ae0dded399915fbfc8c914019762
Fabio Ponciroli <ponch78@gmail.com>
2025-11-07 13:20:31 +0100

    Move package from googlesource to gerritforge
    Following the move to BSL licence the package name
    needed to be moved to away from googlesource.
    
    Change-Id: Id6637cd9b0447e403d06e0cf048d00ccb828003f

---
+++ src/main/java/com/gerritforge/gerrit/plugins/auditsl4j/AuditRecord.java
@@ -0,0 +1,37 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.auditsl4j;
+
+import com.google.gerrit.server.AuditEvent;
+
+public class AuditRecord {
+  public final String type;
+  public final AuditEvent event;
+
+  public AuditRecord(AuditEvent event) {
+    super();
+
+    String eventClass = event.getClass().getName();
+    this.type = eventClass.substring(eventClass.lastIndexOf('.') + 1);
+    this.event = event;
+  }
+
+  public AuditRecord(AuditEvent event, TransformableAuditLogType type) {
+    super();
+
+    this.type = type.name();
+    this.event = event;
+  }
+}
---
+++ src/main/java/com/gerritforge/gerrit/plugins/auditsl4j/AuditUser.java
@@ -0,0 +1,42 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.auditsl4j;
+
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.account.GroupMembership;
+import java.util.Optional;
+
+public class AuditUser extends CurrentUser {
+  String username;
+
+  @Override
+  public GroupMembership getEffectiveGroups() {
+    return null;
+  }
+
+  @Override
+  public Optional<String> getUserName() {
+    return Optional.of(username);
+  }
+
+  public void setUserName(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public Object getCacheKey() {
+    return getAccountId();
+  }
+}
